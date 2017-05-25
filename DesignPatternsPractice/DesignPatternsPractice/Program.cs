4d4f3c5c7c32d4b96066fdd4a9bada679f25b46c
Nikita K <vrolok15@yandex.ru>
2017-05-25 18:38:38 +0300

    Factory Section - Abstract Factory

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -4,43 +4,84 @@ using System.Text;
 
 namespace DesignPatternsPractice
 {
-    
-    public class Point
+
+    //Random Drink Types just for fun :D
+    public class DrinkTypes
     {
-        private double x, y;
+        public string[] TeaType = { "Green Tea", "Earl Grey", "Milk Oolong", "Raspberry Tea" };
+        public string[] CoffeeType = { "Americano", "Cappuccino", "Latte", "Double Double" };
+    }
 
-        //Can be made internal, if need to limit access outside the assembly
-        //If needs to be private, need Inner Factory
-        private Point(double x, double y)
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
         {
-            this.x = x;
-            this.y = y;
+            Random rand = new Random();
+            DrinkTypes drinks = new DrinkTypes();
+            Console.WriteLine($"{drinks.TeaType[rand.Next(0, drinks.TeaType.Length - 1)]} sooths your nerves...");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Random rand = new Random();
+            DrinkTypes drinks = new DrinkTypes();
+            Console.WriteLine($"{drinks.CoffeeType[rand.Next(0, drinks.TeaType.Length - 1)]} energizes you!");
+        }
+    }
+
+    //Factory interface
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"{amount} ml of hot tea are being prepared...");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"{amount} ml of hot coffee are being prepared...");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink { Coffee, Tea }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+            new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            {
+                var factory = (IHotDrinkFactory) Activator.CreateInstance(
+                    Type.GetType("DesignPatternsPractice." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+                );
+                factories.Add(drink, factory);
+            }
         }
 
-        public override string ToString()
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
         {
-            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
-        }
-
-        //Property
-        //creates new point
-        public static Point Origin => new Point(0, 0);
-
-        //Signleton field
-        //initializes static field once
-        public static Point Origin2 = new Point(0, 0); //better
-
-        public class Factory
-        {
-            public static Point NewCartesianPoint(double x, double y)
-            {
-                return new Point(x, y);
-            }
-
-            public static Point NewPolarPoint(double rho, double theta)
-            {
-                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-            }
+            return factories[drink].Prepare(amount);
         }
     }
 
@@ -48,11 +89,14 @@ namespace DesignPatternsPractice
     {
         static void Main(string[] args)
         {
-            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
-            Console.WriteLine(point);
-            Console.ReadLine();
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 250);
+            drink.Consume();
 
-            var origin = Point.Origin;
+            var drink2 = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 150);
+            drink2.Consume();
+
+            Console.ReadLine();
         }
     }
 }
