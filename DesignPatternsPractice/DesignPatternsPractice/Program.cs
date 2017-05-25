b4080c2e6d41fcb62b85e4b052ef324f4c17e1c7
Nikita K <vrolok15@yandex.ru>
2017-05-26 00:50:53 +0300

    Factory Section - Coding Excercise (Completed)

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -4,139 +4,46 @@ using System.Text;
 
 namespace DesignPatternsPractice
 {
-    public interface IHotDrink
+    public class Person
     {
-        void Consume();
-    }
+        public int Id { get; set; }
+        public static int IdSetter = 0;
+        public string Name { get; set; }
 
-    internal class Tea : IHotDrink
-    {
-        public void Consume()
+        public Person(string name)
         {
-            Console.WriteLine("Warm Tea sooths your nerves...");
+            Id = IdSetter;
+            IdSetter++;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Person ID: {Id}, Name: {Name}";
         }
     }
 
-    internal class Coffee : IHotDrink
+    public class PersonFactory
     {
-        public void Consume()
+        public static Person NewPerson(string name)
         {
-            Console.WriteLine("Hot Coffee energizes you!");
+            return new Person(name);
         }
-    }
-
-    //Factory interface
-    public interface IHotDrinkFactory
-    {
-        IHotDrink Prepare(int amount);
-    }
-
-    internal class TeaFactory : IHotDrinkFactory
-    {
-        public IHotDrink Prepare(int amount)
-        {
-            Console.WriteLine($"{amount} ml of Warm Tea are being prepared...");
-            Console.WriteLine("");
-            return new Tea();
-        }
-    }
-
-    internal class CoffeeFactory : IHotDrinkFactory
-    {
-        public IHotDrink Prepare(int amount)
-        {
-            Console.WriteLine($"{amount} ml of Hot Coffee are being prepared...");
-            Console.WriteLine("");
-            return new Coffee();
-        }
-    }
-
-    public class HotDrinkMachine
-    {
-        //!!!
-        // Original Class was bad, because it used enum to access drink types
-        // That meant that variety of available drinks could only be changed from inside
-        // Which is not good! Remember Open/Closed Principle!
-        //!!!
-
-        //public enum AvailableDrink { Coffee, Tea }
-
-        //private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
-        //    new Dictionary<AvailableDrink, IHotDrinkFactory>();
-
-        //public HotDrinkMachine()
-        //{
-        //    foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
-        //    {
-        //        var factory = (IHotDrinkFactory) Activator.CreateInstance(
-        //            Type.GetType("DesignPatternsPractice." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
-        //        );
-        //        factories.Add(drink, factory);
-        //    }
-        //}
-
-        //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
-        //{
-        //    return factories[drink].Prepare(amount);
-        //}
-
-        private List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
-
-        public HotDrinkMachine()
-        {
-            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
-            {
-                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
-                {
-                    factories.Add(Tuple.Create(
-                        t.Name.Replace("Factory", string.Empty),
-                        (IHotDrinkFactory)Activator.CreateInstance(t)
-                        ));
-                }
-            }
-        }
-
-        public IHotDrink MakeDrink()
-        {
-            Console.WriteLine("Available drinks:");
-            for (var index = 0; index < factories.Count; index++)
-            {
-                var tuple = factories[index];
-                Console.WriteLine($"{index}: {tuple.Item1}");
-            }
-            while (true)
-            {
-                string s;
-                if ((s = Console.ReadLine()) != null
-                    && int.TryParse(s, out int i)
-                    && i >= 0
-                    && i < factories.Count)
-                {
-                    Console.Write("Specify amount:");
-                    if ((s = Console.ReadLine()) != null
-                        && int.TryParse(s, out int amount)
-                        && amount >= 0)
-                    {
-                        return factories[i].Item2.Prepare(amount);
-                    }
-                }
-
-                Console.WriteLine("Incorrect input, try again");
-            }
-        }
+        
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var machine = new HotDrinkMachine();
-            var drink = machine.MakeDrink();
-            drink.Consume();
-
-            //var drink2 = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 150);
-            //drink2.Consume();
-
+            var person = PersonFactory.NewPerson("Johnny");
+            var person2 = PersonFactory.NewPerson("Joey");
+            var person3 = PersonFactory.NewPerson("Dee Dee");
+            var person4 = PersonFactory.NewPerson("Tommy");
+            Console.WriteLine(person);
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+            Console.WriteLine(person4);
             Console.ReadLine();
         }
     }
