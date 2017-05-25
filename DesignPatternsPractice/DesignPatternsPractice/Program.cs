124a4540bdd21f22c28cd666d5fe6f014e4c36b4
Nikita K <vrolok15@yandex.ru>
2017-05-25 23:42:31 +0300

    Factory Section - Abstract Factory fixed to follow Open/Closed Principle

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -4,14 +4,6 @@ using System.Text;
 
 namespace DesignPatternsPractice
 {
-
-    //Random Drink Types just for fun :D
-    public class DrinkTypes
-    {
-        public string[] TeaType = { "Green Tea", "Earl Grey", "Milk Oolong", "Raspberry Tea" };
-        public string[] CoffeeType = { "Americano", "Cappuccino", "Latte", "Double Double" };
-    }
-
     public interface IHotDrink
     {
         void Consume();
@@ -21,9 +13,7 @@ namespace DesignPatternsPractice
     {
         public void Consume()
         {
-            Random rand = new Random();
-            DrinkTypes drinks = new DrinkTypes();
-            Console.WriteLine($"{drinks.TeaType[rand.Next(0, drinks.TeaType.Length - 1)]} sooths your nerves...");
+            Console.WriteLine("Warm Tea sooths your nerves...");
         }
     }
 
@@ -31,9 +21,7 @@ namespace DesignPatternsPractice
     {
         public void Consume()
         {
-            Random rand = new Random();
-            DrinkTypes drinks = new DrinkTypes();
-            Console.WriteLine($"{drinks.CoffeeType[rand.Next(0, drinks.TeaType.Length - 1)]} energizes you!");
+            Console.WriteLine("Hot Coffee energizes you!");
         }
     }
 
@@ -47,7 +35,8 @@ namespace DesignPatternsPractice
     {
         public IHotDrink Prepare(int amount)
         {
-            Console.WriteLine($"{amount} ml of hot tea are being prepared...");
+            Console.WriteLine($"{amount} ml of Warm Tea are being prepared...");
+            Console.WriteLine("");
             return new Tea();
         }
     }
@@ -56,32 +45,84 @@ namespace DesignPatternsPractice
     {
         public IHotDrink Prepare(int amount)
         {
-            Console.WriteLine($"{amount} ml of hot coffee are being prepared...");
+            Console.WriteLine($"{amount} ml of Hot Coffee are being prepared...");
+            Console.WriteLine("");
             return new Coffee();
         }
     }
 
     public class HotDrinkMachine
     {
-        public enum AvailableDrink { Coffee, Tea }
+        //!!!
+        // Original Class was bad, because it used enum to access drink types
+        // That meant that variety of available drinks could only be changed from inside
+        // Which is not good! Remember Open/Closed Principle!
+        //!!!
 
-        private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
-            new Dictionary<AvailableDrink, IHotDrinkFactory>();
+        //public enum AvailableDrink { Coffee, Tea }
+
+        //private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+        //    new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        //public HotDrinkMachine()
+        //{
+        //    foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //    {
+        //        var factory = (IHotDrinkFactory) Activator.CreateInstance(
+        //            Type.GetType("DesignPatternsPractice." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+        //        );
+        //        factories.Add(drink, factory);
+        //    }
+        //}
+
+        //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        //{
+        //    return factories[drink].Prepare(amount);
+        //}
+
+        private List<Tuple<string, IHotDrinkFactory>> factories = new List<Tuple<string, IHotDrinkFactory>>();
 
         public HotDrinkMachine()
         {
-            foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
             {
-                var factory = (IHotDrinkFactory) Activator.CreateInstance(
-                    Type.GetType("DesignPatternsPractice." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
-                );
-                factories.Add(drink, factory);
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+                {
+                    factories.Add(Tuple.Create(
+                        t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory)Activator.CreateInstance(t)
+                        ));
+                }
             }
         }
 
-        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        public IHotDrink MakeDrink()
         {
-            return factories[drink].Prepare(amount);
+            Console.WriteLine("Available drinks:");
+            for (var index = 0; index < factories.Count; index++)
+            {
+                var tuple = factories[index];
+                Console.WriteLine($"{index}: {tuple.Item1}");
+            }
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null
+                    && int.TryParse(s, out int i)
+                    && i >= 0
+                    && i < factories.Count)
+                {
+                    Console.Write("Specify amount:");
+                    if ((s = Console.ReadLine()) != null
+                        && int.TryParse(s, out int amount)
+                        && amount >= 0)
+                    {
+                        return factories[i].Item2.Prepare(amount);
+                    }
+                }
+
+                Console.WriteLine("Incorrect input, try again");
+            }
         }
     }
 
@@ -90,11 +131,11 @@ namespace DesignPatternsPractice
         static void Main(string[] args)
         {
             var machine = new HotDrinkMachine();
-            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 250);
+            var drink = machine.MakeDrink();
             drink.Consume();
 
-            var drink2 = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 150);
-            drink2.Consume();
+            //var drink2 = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 150);
+            //drink2.Consume();
 
             Console.ReadLine();
         }
