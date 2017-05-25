d47970ba6bd9d3a529aed6060ff85499bf981b2b
Nikita K <vrolok15@yandex.ru>
2017-05-25 12:26:16 +0300

    Builder Section - Added Html Builder

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -6,10 +6,86 @@ using System.Threading.Tasks;
 
 namespace DesignPatternsPractice
 {
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int IndentSize = 2;
+
+        public HtmlElement()
+        {
+            
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', IndentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', IndentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        private HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement {Name = rootName};
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //Old method without Html Builder
+            Console.WriteLine("< !--Old method without Html Builder-- >");
+            Console.WriteLine("");
+
             var hello = "hello";
             var sb = new StringBuilder();
             sb.Append("<p>");
@@ -26,6 +102,16 @@ namespace DesignPatternsPractice
             }
             sb.Append("</ul>");
             Console.WriteLine(sb);
+            Console.WriteLine("");
+
+            //New method with the Html Builder
+            Console.WriteLine("< !--New method with the Html Builder-- >");
+            Console.WriteLine("");
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Hello");
+            builder.AddChild("li", "World");
+            Console.WriteLine(builder.ToString());
+
             Console.ReadLine();
         }
     }
