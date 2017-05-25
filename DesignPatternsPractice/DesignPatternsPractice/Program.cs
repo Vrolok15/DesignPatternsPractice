b3a411b1ae3d081c7699027f0a8599ac2ed60599
Nikita K <vrolok15@yandex.ru>
2017-05-25 17:08:11 +0300

    Factory Section - Moved Factory methods into Factory class

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -10,7 +10,7 @@ namespace DesignPatternsPractice
         Polar
     }
 
-    public class Point
+    public class PointFactory 
     {
         public static Point NewCartesianPoint(double x, double y)
         {
@@ -21,10 +21,13 @@ namespace DesignPatternsPractice
         {
             return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
         }
+    }
 
+    public class Point
+    {
         private double x, y;
 
-        private Point(double x, double y)
+        public Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -40,7 +43,7 @@ namespace DesignPatternsPractice
     {
         static void Main(string[] args)
         {
-            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
             Console.WriteLine(point);
             Console.ReadLine();
         }
