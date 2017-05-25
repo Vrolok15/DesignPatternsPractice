c6fdc7f2d213263962b3a76bc8944fd21afe81a3
Nikita K <vrolok15@yandex.ru>
2017-05-25 11:48:04 +0300

    Builder Section - No Builder

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -10,6 +10,23 @@ namespace DesignPatternsPractice
     {
         static void Main(string[] args)
         {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "Hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+            Console.ReadLine();
         }
     }
 }
