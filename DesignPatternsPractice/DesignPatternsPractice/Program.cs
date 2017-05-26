add819117ba6289d8b0f497c94d81a2c2851f058
Nikita K <vrolok15@yandex.ru>
2017-05-26 21:04:08 +0300

    Singleton Section - Singleton with Lazy Loading

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -1,94 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
+using MoreLinq;
 
 namespace DesignPatternsPractice
 {
-
-    public static class ExtensionMethods
+    public interface IDatabase
     {
-
-        public static T DeepCopyXml<T>(this T self)
-        {
-            using (var ms = new MemoryStream())
-            {
-                var s = new XmlSerializer(typeof(T));
-                s.Serialize(ms, self);
-                //does same as stream.Seek(0, SeekOrigin.Begin);
-                ms.Position = 0;
-                return (T) s.Deserialize(ms);
-            }
-                
-        }
+        int GetPopulation(string name);
     }
 
-    public class Person
+
+
+    public class SingletonDatabase : IDatabase
     {
-        public string[] Names;
-        public Address Address;
+        private Dictionary<string, int> capitals;
 
-        //Need to have empty constructors to use XmlSerializer
-        public Person()
+        private SingletonDatabase()
         {
-            
+            Console.WriteLine("Initializing Database");
+            // Read lines from a file to simulate Database access
+            capitals = File.ReadAllLines("capitals.txt")
+                // Group lines by 2
+                .Batch(2)
+                // Transform them to dictionary entries
+                .ToDictionary(
+                // Where 1st line goes to key value (trimmed - without spaces)
+                list => list.ElementAt(0).Trim(),
+                // And second line parses into int
+                list => int.Parse(list.ElementAt(1))
+                );
+        }
+        
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
         }
 
-        public Person(string[] names, Address address)
-        {
-            Names = names ?? throw new ArgumentNullException(paramName: nameof(names));
-            Address = address ?? throw new ArgumentNullException(paramName: nameof(address));
-        }
+        private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
 
-        public interface IInter
-        {
-            void Stop();
-            string Name { get; set; }
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
-        }
-
-    }
-
-    public class Address
-    {
-        public string StreetName;
-        public int HouseNumber;
-
-        //Need to have empty constructors to use XmlSerializer
-        public Address()
-        {
-            
-        }
-
-        public Address(string streetName, int houseNumber)
-        {
-            StreetName = streetName ?? throw new ArgumentNullException(paramName: nameof(streetName));
-            HouseNumber = houseNumber;
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
-        }
+        public static SingletonDatabase Instance => instance.Value;
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var john = new Person(new [] {"John", "Smith"}, 
-                new Address("Lundy's Lane", 1234));
-
-            var jane = john.DeepCopyXml();
-            jane.Names = new[] {"Jane", "Black"};
-            jane.Address.HouseNumber = 4321;
-
-            Console.WriteLine(john);
-            Console.WriteLine(jane);
+            var db = SingletonDatabase.Instance;
+            var city = "Tokyo";
+            Console.WriteLine($"{city} has population: {db.GetPopulation(city)}");
             Console.ReadLine();
         }
     }
---
+++ Test/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+
+        public static int HowManyRepeatingNumbersInArray(int[] array)
+        {
+            HashSet<int> differentNumbersSet = new HashSet<int>();
+            HashSet<int> repeatingSet = new HashSet<int>();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (differentNumbersSet.Contains(array[i]))
+                {
+                    repeatingSet.Add(array[i]);
+                }
+                else
+                {
+                    differentNumbersSet.Add(array[i]);
+                }
+                    
+            }
+
+            return repeatingSet.Count;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 1, 1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 7, 8, 9, 9 };
+            Console.WriteLine(HowManyRepeatingNumbersInArray(array) + " total Repeating numbers");
+            Console.ReadLine();
+        }
+    }
+}
