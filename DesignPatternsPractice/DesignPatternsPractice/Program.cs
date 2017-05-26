016c348ba28abfbab78d1339b4b3f2f6ba7470fd
Nikita K <vrolok15@yandex.ru>
2017-05-27 00:50:55 +0300

    Singleton Section - Dependency Injection and Singleton

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
+using Autofac;
 using MoreLinq;
 using NUnit.Framework;
 
@@ -51,6 +52,35 @@ namespace DesignPatternsPractice
         public static SingletonDatabase Instance => instance.Value;
     }
 
+    public class OrdinaryDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+        private static int instanceCount; //0
+        public static int Count => instanceCount;
+
+        public OrdinaryDatabase()
+        {
+            Console.WriteLine("Initializing Database");
+            // Read lines from a file to simulate Database access
+            capitals = File.ReadAllLines("C:\\Кабинет\\VS\\DesignPatternsPractice\\DesignPatternsPractice\\DesignPatternsPractice\\capitals.txt")
+                // Group lines by 2
+                .Batch(2)
+                // Transform them to dictionary entries
+                .ToDictionary(
+                    // Where 1st line goes to key value (trimmed - without spaces)
+                    list => list.ElementAt(0).Trim(),
+                    // And second line parses into int
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+    }
+
     public class SingletonRecordFinder
     {
         public int GetTotalPopulation(IEnumerable<String> names)
@@ -59,6 +89,39 @@ namespace DesignPatternsPractice
             foreach (var name in names)
             {
                 result += SingletonDatabase.Instance.GetPopulation(name);
+            }
+            return result;
+        }
+    }
+
+    public class DummyDatabase : IDatabase
+    {
+        public int GetPopulation(string name)
+        {
+            return new Dictionary<string, int>
+            { 
+            ["alpha"] = 1,
+            ["beta"] = 2,
+            ["gamma"] = 3
+            }[name];
+        }
+    }
+
+    public class ConfigurableRecordFinder
+    {
+        private IDatabase database;
+
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            this.database = database ?? throw new ArgumentNullException(paramName: nameof(database));
+        }
+
+        public int GetTotalPopulation(IEnumerable<String> names)
+        {
+            var result = 0;
+            foreach (var name in names)
+            {
+                result += database.GetPopulation(name);
             }
             return result;
         }
@@ -84,6 +147,30 @@ namespace DesignPatternsPractice
             var names = new[] {"Seoul", "Mexico City"};
             int tp = rf.GetTotalPopulation(names);
             Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+        }
+
+        [Test]
+        public void ConfigurablePopulationTest()
+        {
+            var rf = new ConfigurableRecordFinder(new DummyDatabase());
+            var names = new[] { "alpha", "gamma" };
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void DIPopulationTest()
+        {
+            var cb = new ContainerBuilder();
+            // Can change OrdinaryDatabase to DummyDatabase for testing!
+            cb.RegisterType<OrdinaryDatabase>()
+                .As<IDatabase>()
+                .SingleInstance();
+            cb.RegisterType<ConfigurableRecordFinder>();
+            using (var c = cb.Build())
+            {
+                var rf = c.Resolve<ConfigurableRecordFinder>();
+            }
         }
     }
 
