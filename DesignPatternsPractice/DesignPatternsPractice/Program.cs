6db4134614d32a3c9e5aac43ff34a4231387d49f
Nikita K <vrolok15@yandex.ru>
2017-05-26 15:22:33 +0300

    Prototype Section - Using ICloneable is bad! (Shallow Copy)

--- DesignPatternsPractice/DesignPatternsPractice/Program.cs
+++ DesignPatternsPractice/DesignPatternsPractice/Program.cs
@@ -4,46 +4,65 @@ using System.Text;
 
 namespace DesignPatternsPractice
 {
-    public class Person
+    public class Person : ICloneable
     {
-        public int Id { get; set; }
-        public static int IdSetter = 0;
-        public string Name { get; set; }
+        public string[] Names;
+        public Address Address;
 
-        public Person(string name)
+        public Person(string[] names, Address address)
         {
-            Id = IdSetter;
-            IdSetter++;
-            Name = name;
+            Names = names ?? throw new ArgumentNullException(paramName: nameof(names));
+            Address = address ?? throw new ArgumentNullException(paramName: nameof(address));
+        }
+
+        // Returns only an object!
+        public object Clone()
+        {
+            return new Person(Names, (Address)Address.Clone());
         }
 
         public override string ToString()
         {
-            return $"Person ID: {Id}, Name: {Name}";
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
         }
     }
 
-    public class PersonFactory
+    public class Address : ICloneable
     {
-        public static Person NewPerson(string name)
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
         {
-            return new Person(name);
+            StreetName = streetName ?? throw new ArgumentNullException(paramName: nameof(streetName));
+            HouseNumber = houseNumber;
         }
-        
+
+        // Need to clone the Address too!
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var person = PersonFactory.NewPerson("Johnny");
-            var person2 = PersonFactory.NewPerson("Joey");
-            var person3 = PersonFactory.NewPerson("Dee Dee");
-            var person4 = PersonFactory.NewPerson("Tommy");
-            Console.WriteLine(person);
-            Console.WriteLine(person2);
-            Console.WriteLine(person3);
-            Console.WriteLine(person4);
+            var john = new Person(new [] {"John", "Smith"}, 
+                new Address("Lundy's Lane", 1234));
+            var jane = (Person)john.Clone();
+
+            jane.Names = new[] {"Jane", "Black"};
+            jane.Address.HouseNumber = 4321;
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
             Console.ReadLine();
         }
     }
